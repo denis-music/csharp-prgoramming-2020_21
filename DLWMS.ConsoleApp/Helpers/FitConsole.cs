2ad05b70d083f2a70d0964416325ac612b68fb0d
Denis Music <denis@edu.fit.ba>
2020-10-12 20:27:01 +0200

    Add project files.

---
+++ DLWMS.ConsoleApp/Helpers/FitConsole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DLWMS.ConsoleApp
+{
+    public class FitConsole
+    {
+        public void WriteLine(string content) { }
+    }
+}
---
+++ DLWMS.DataAccess/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DLWMS.DataAccess
+{
+    public class Student
+    {
+        int _indeks;     
+        public int Indeks { 
+            get { return _indeks; } 
+            set { 
+                if(value>0 && value<300000)
+                    _indeks = value; 
+            } 
+        }     
+        public string Ime { get; set; }
+        public string Prezime { get; private set; }
+
+        public Student(int indeks, string ime, string prezime)
+        {
+            Indeks = indeks;
+            Ime = ime;
+            Prezime = prezime;
+        }
+
+        public override string ToString()
+        {
+            return $"({Indeks}) {Ime} {Prezime}";
+        }
+    }
+}
